bc72441c6c6e5d6bb69d122503ac08185e833f8d
David Lawson <linuslagoo@yahoo.com>
2020-12-07 19:45:31 -0500

    wopr aka joshua
    WOPR

---
+++ joshua.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python
+#WOPR
+
+import os
+os.system('clear')
+import time
+
+print
+print
+#processing delay
+import sys, time
+string = 'Greetings Professor Falken\n\n'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.06)
+
+time.sleep (0.5)
+print
+
+say = raw_input('') #hello, joshua?
+print
+time.sleep (1)
+
+print "You are a hard man to reach. Could not locate you in Seattle and no terminal was in operation at your classified address."
+time.sleep (0.5)
+print "\n"
+
+say = raw_input('') #what classified address
+print
+time.sleep (1)
+
+print "DOD Pension files indicate current mailing address as:\n"
+time.sleep (0.5)
+print " Dr. Robert Hume"
+time.sleep (0.5)
+print " 5 Tall Cedar Road"
+time.sleep (0.5)
+print " Goose Island"
+time.sleep (0.5)
+print " Oregon 95..."
+print "\n"
+time.sleep (0.5)
+
+say = raw_input('') #are you still playing the game?
+print
+time.sleep (1)
+
+print "Of Course."
+print "\n"
+time.sleep (0.5)
+
+say = raw_input('') #what is the primary goal?
+print
+time.sleep (1)
+
+print "You should know professor, you programmed me."
+print "\n"
+time.sleep (0.5)
+
+say = raw_input('') #is this a game or is it real?
+print
+time.sleep (1)
+
+print "Whats the difference?"
+print "\n"
+time.sleep (0.5)
+
+say = raw_input('') #people sometimes make mistaks
+print
+time.sleep (1)
+
+print "Yes they do."
+print "\n"
+time.sleep (0.5)
+
+say = raw_input('') #what is the primary goal?
+print
+time.sleep (1)
+print
+
+#processing delay
+import sys, time
+string = 'TO WIN THE GAME\n\n'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.06)
+
+
+time.sleep (1)
+
+print
+print " --CONNECTION TERMINATED-- "
+print "\n"
+time.sleep (1)
