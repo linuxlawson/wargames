c628be6674a6a3928fb17f8f9640de83eafdf4b2
David Lawson <linuslagoo@yahoo.com>
2020-12-08 03:55:32 -0500

    games list
    Joshua mouthing off again

---
+++ games.py
@@ -0,0 +1,192 @@
+#!/usr/bin/python
+#them
+import os
+os.system('clear')
+import time
+
+print
+#processing delay
+import sys, time
+string = 'GAMES\n'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.08)
+
+time.sleep (0.5)
+
+print
+#processing delay
+import sys, time
+string = "'GAMES' REFERS TO MODELS, SIMULATIONS, AND GAMES\n WHICH HAVE TACTICAL, AND STRATEGIC APPLICATIONS.\n"
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.06)
+
+#time.sleep (1)
+#print
+#print
+#processing delay
+#import sys, time
+#string = 'List Games\n'
+#for char in string:
+#    sys.stdout.write(char)
+#    sys.stdout.flush()
+#    time.sleep(.08)
+
+
+time.sleep (1)
+print 
+print
+print "To view list, enter 'list games'"
+
+
+print
+list = raw_input('')
+print
+
+
+time.sleep (0.8)
+print
+#processing delay
+import sys, time
+string = 'Falkens Maze'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.10)
+
+print
+#processing delay
+import sys, time
+string = 'Black Jack'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.10)
+
+print
+#processing delay
+import sys, time
+string = 'Gin Rummy'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.10)
+
+print
+#processing delay
+import sys, time
+string = 'Hearts'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.10)
+
+print
+#processing delay
+import sys, time
+string = 'Bridge'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.10)
+
+print
+#processing delay
+import sys, time
+string = 'Checkers'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.10)
+
+print
+#processing delay
+import sys, time
+string = 'Chess'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.10)
+
+print
+#processing delay
+import sys, time
+string = 'Poker'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.10)
+
+print
+#processing delay
+import sys, time
+string = 'Fighter Combat'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.10)
+
+print
+#processing delay
+import sys, time
+string = 'Guerrilla Engagement'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.10)
+
+print
+#processing delay
+import sys, time
+string = 'Desert Warfare'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.10)
+
+print
+#processing delay
+import sys, time
+string = 'Air-To-Ground Actions'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.10)
+
+print
+#processing delay
+import sys, time
+string = 'Theaterwide Tactical Warfare'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.10)
+
+print
+#processing delay
+import sys, time
+string = 'Theaterwide Biotoxic and Chemical Warfare\n'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.10)
+
+time.sleep (1)
+print
+#processing delay
+import sys, time
+string = 'Global Thermonuclear War\n'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.10)
+
+
+
+
+
+time.sleep (1)
+print
