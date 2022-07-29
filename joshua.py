f90392e4f2263a72f803f926ea59a5ccd97de7f2
David Lawson <linuslagoo@yahoo.com>
2022-07-29 01:24:03 -0400

    combined
    combined the 2 files, little better

---
+++ joshua.py
@@ -0,0 +1,280 @@
+#!/usr/bin/env python3
+#WOPR
+
+import os
+os.system('clear')
+import time
+
+
+twhite = '\033[37m'
+endc = '\033[m'
+
+print
+print
+#processing delay
+import sys, time
+string = (twhite + 'Greetings Professor Falken\n\n' + endc)
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.06)
+
+time.sleep (0.5)
+print
+
+say = input('') #hello, joshua?
+print
+time.sleep (1)
+
+print (twhite + "You are a hard man to reach. Could not locate you in Seattle and no terminal was in operation at your classified address." + endc)
+time.sleep (0.5)
+print ("\n")
+
+say = input('') #what classified address
+print
+time.sleep (1)
+
+print (twhite + "DOD Pension files indicate current mailing address as:\n" + endc)
+time.sleep (0.5)
+print (twhite + " Dr. Robert Hume" + endc)
+time.sleep (0.5)
+print (twhite + " 5 Tall Cedar Road" + endc)
+time.sleep (0.5)
+print (twhite + " Goose Island" + endc)
+time.sleep (0.5)
+print (twhite + " Oregon 95..." + endc)
+print ("\n")
+time.sleep (0.5)
+
+say = input('') #are you still playing the game?
+print
+time.sleep (1)
+
+print (twhite + "Of Course." + endc)
+print ("\n")
+time.sleep (0.5)
+
+say = input('') #what is the primary goal?
+print
+time.sleep (1)
+
+print (twhite + "You should know professor, you programmed me." + endc)
+print ("\n")
+time.sleep (0.5)
+
+say = input('') #is this a game or is it real?
+print
+time.sleep (1)
+
+print (twhite + "Whats the difference?" + endc)
+print ("\n")
+time.sleep (0.5)
+
+say = input('') #people sometimes make mistaks
+print
+time.sleep (1)
+
+print (twhite + "Yes they do." + endc)
+print ("\n")
+time.sleep (0.5)
+
+say = input('') #what is the primary goal?
+print
+time.sleep (1)
+print
+
+#processing delay
+import sys, time
+string = (twhite + 'TO WIN THE GAME\n\n' + endc)
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.06)
+
+time.sleep (1)
+print 
+print
+print ("To view games, enter 'games'")
+
+print ('\n')
+list = input('')
+print ('\n\n')
+
+os.system('clear')
+time.sleep (1)
+print
+#processing delay
+import sys, time
+string = 'GAMES\n'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.08)
+
+time.sleep (0.5)
+
+print
+#processing delay
+import sys, time
+string = "'GAMES' REFERS TO MODELS, SIMULATIONS, AND GAMES\n WHICH HAVE TACTICAL, AND STRATEGIC APPLICATIONS.\n\n"
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.06)
+
+#os.system('clear')
+time.sleep (1)
+print 
+print
+print ("To view list, enter 'list games'")
+
+
+print ('\n')
+list = input('')
+print
+
+os.system('clear')
+time.sleep (0.8)
+print ('')
+#processing delay
+import sys, time
+string = 'Falkens Maze\n'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.10)
+
+print
+#processing delay
+import sys, time
+string = 'Black Jack\n'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.10)
+
+print
+#processing delay
+import sys, time
+string = 'Gin Rummy\n'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.10)
+
+print
+#processing delay
+import sys, time
+string = 'Hearts\n'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.10)
+
+print
+#processing delay
+import sys, time
+string = 'Bridge\n'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.10)
+
+print
+#processing delay
+import sys, time
+string = 'Checkers\n'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.10)
+
+print
+#processing delay
+import sys, time
+string = 'Chess\n'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.10)
+
+print
+#processing delay
+import sys, time
+string = 'Poker\n'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.10)
+
+print
+#processing delay
+import sys, time
+string = 'Fighter Combat\n'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.10)
+
+print
+#processing delay
+import sys, time
+string = 'Guerrilla Engagement\n'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.10)
+
+print
+#processing delay
+import sys, time
+string = 'Desert Warfare\n'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.10)
+
+print
+#processing delay
+import sys, time
+string = 'Air-To-Ground Actions\n'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.10)
+
+print
+#processing delay
+import sys, time
+string = 'Theaterwide Tactical Warfare\n'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.10)
+
+print
+#processing delay
+import sys, time
+string = 'Theaterwide Biotoxic and Chemical Warfare\n\n'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.10)
+
+time.sleep (1)
+print
+#processing delay
+import sys, time
+string = 'Global Thermonuclear War\n\n'
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.10)
+
+
+time.sleep (3)
+
+print ('\n')
+print (twhite +" --CONNECTION TERMINATED-- " + endc)
+print ("\n")
+time.sleep (1)
