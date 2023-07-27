5d251973090ccd46b8c4fe0bee23faeef82d4e94
David <linuslagoo@yahoo.com>
2023-07-27 19:11:09 -0400

    launch code

--- launch.py
+++ launch.py
@@ -13,24 +13,26 @@ white = '\033[37m'
 endc = '\033[m'
 bold = "\033[1m"
 
-def launch_code():
-    time.sleep (0.5)
-target = ("CPE 1704 TKS")
-time.sleep (0.5)
+def launch_codes():
+    print ("")
+os.system('clear')
 print ("")
+time.sleep (0.5)
 
+target = ("CPE 1704 TKS")
 iscaps = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 isnums = list("1234567890")
 isspace = list(" ")
 
-attemptThis = ''.join(random.choice(iscaps + isnums + isspace) for i in range(len(target)))
+attemptThis = ''.join(random.choice(iscaps + isnums + isspace) 
+    for _ in range(len(target)))
 attemptNext = ''
 
 completed = False
 generation = 0
 
-while completed == False:
-    print(' ',bold + white + attemptThis,'\033[F' + endc)
+while not completed:
+    print(' ', bold + white + attemptThis, end='\r' + endc)
     attemptNext = ''
     completed = True
     for i in range(len(target)):
@@ -42,22 +44,24 @@ while completed == False:
     generation += 1
     attemptThis = attemptNext
     time.sleep(0.1)
-launch_code()
 
 
 time.sleep (1)
-print ("\n\n\n  Launch Code Authenticated")
+print ("\n\n\n  Launch Code Authentic")
 time.sleep (0.8)
 print ("\n  Launch Order Confirmed")
 time.sleep (0.8)
 print ("\n  Launch missles in T-minus " + str(generation) + " second(s)\n")
 time.sleep (0.5)
-print(white + '\n  -------------------------' + endc)
+print(f"{white}\n  -------------------------{endc}")
 time.sleep (0.2)
-print(white + '\n  -------------------------' + endc)
+print(f"{white}\n  -------------------------{endc}")
 time.sleep (0.2)
-print ("\n  'Turn your key, sir.'\n")
+print ("\n  Turn your key, sir. \n")
 time.sleep (1)
 print("")
+#launch_codes()
+
+
 
 
